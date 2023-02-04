d7baeaa8413a111207fd644fe58f8cb80c80d7e3
SergioAJS <SergFanMadrid@gmail.com>
2023-02-04 19:18:07 +0500

    feat: add footer style, add header style behaviour on scroll

--- gf-webapp/src/component/pageBorder/PageBorder.tsx
+++ gf-webapp/src/component/pageBorder/PageBorder.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./PageBorder.module.scss";
 import logo from "../../resources/icons/logo.svg";
 import {Slider} from "../sliderImage/Slider";
 import {SliderVideo} from "../sliderVideo/SliderVideo";
+import footerImage from "../../resources/sliderImages/landscape_mountains_sun_140434_1920x1080.jpg";
+import {useScrollPosition} from "../../hooks/useScrollPosition";
 
 export function PageBorder() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+  const scrollPosition = useScrollPosition();
+
   return (
     <div className={styles.wrapper}>
-      <header className={styles.header}>
+      <header className={scrollPosition > 100 ? styles.header_scroll : styles.header}>
         <nav className={styles.nav}>
           <ul className={styles.list}>
             <li>
@@ -26,8 +34,15 @@ export function PageBorder() {
                 alt="Golden Fleece logo"
               />
             </li>
-            <li>
-              EN
+            <li className={styles.languages}
+              onClick={handleOpen}
+            >
+              <p className={styles.english}>
+                EN
+              </p>
+              {open ? (<p className={`${styles.hidden} ${styles.russian}`}>
+                RU
+              </p>) : null}
             </li>
             <li>
               Book now
@@ -39,7 +54,14 @@ export function PageBorder() {
         <Slider />
         <SliderVideo />
       </main>
-      <footer className={styles.footer} />
+      <footer className={styles.footer}>
+        <div>
+          Footer
+        </div>
+        <img src={footerImage}
+          alt="footer image"
+        />
+      </footer>
     </div>
   );
 }
