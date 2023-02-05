f4521d6ccbfdf2da2a936f5067b717cb68b9e5d7
Andrew <scorpigg@yandex.ru>
2023-02-05 17:26:25 +0400

    feat: implement borders of the map

--- gf-webapp/src/component/map/Map.tsx
+++ gf-webapp/src/component/map/Map.tsx
@@ -11,49 +11,51 @@ export const Map = () => {
       id: 1,
       name: "Golden fleece",
       x: 523,
-      y: 613,
+      y: 600,
       markerType: "main",
+      // eslint-disable-next-line max-len
+      descriprion: "Located in Kutaisi, 4.9 km from WhiteBridge, Hotel Golden Fleece provides accommodation with a seasonal outdoor swimming pool, free private parking, a terrace and a bar",
     },
     {
       id: 2,
       name: "Cake club",
       x: 645,
-      y: 690,
+      y: 670,
       markerType: "food",
     },
     {
       id: 3,
       name: "Shedevri",
       x: 680,
-      y: 370,
+      y: 350,
       markerType: "food",
     },
     {
       id: 4,
       name: "Green yard",
       x: 840,
-      y: 450,
+      y: 430,
       markerType: "food",
     },
     {
       id: 5,
       name: "Youth park",
       x: 580,
-      y: 460,
+      y: 440,
       markerType: "park",
     },
     {
       id: 6,
       name: "Tennis cort",
       x: 724,
-      y: 189,
+      y: 170,
       markerType: "park",
     },
     {
       id: 7,
       name: "Ramaz Shengelia Stadium",
       x: 680,
-      y: 110,
+      y: 90,
       markerType: "park",
     },
   ];
@@ -63,20 +65,45 @@ export const Map = () => {
 
   const mouseDownHandler = (e: React.MouseEvent) => {
     const target = e.target as HTMLElement;
-    setDragging(true);
-    setStartX(e.pageX - Number.parseInt(target.style.left || "0"));
-    setStartY(e.pageY - Number.parseInt(target.style.top || "0"));
+    if (target.dataset.name === "map-content") {
+      setDragging(true);
+    }
+    const style = window.getComputedStyle(target);
+    const transform = new DOMMatrixReadOnly(style.transform);
+    const translateX = transform.m41;
+    const translateY = transform.m42;
+    setStartX(e.pageX - translateX);
+    setStartY(e.pageY - translateY);
   };
 
+
   const mouseMoveHandler = (e: React.MouseEvent) => {
-    const target = e.target as HTMLElement;
     if (!dragging) {
       return;
     }
+    const target = e.target as HTMLElement;
+    const LEFT = 0;
+    const BOTTOM = 0;
+    const TOP = target.offsetHeight - (target.parentElement?.offsetHeight || 0);
+    const RIGTH = (target.parentElement?.offsetWidth || 0) - target.offsetWidth;
 
     if (target.dataset.name === "map-content") {
-      target.style.top = `${e.pageY - startY}px`;
-      target.style.left = `${e.pageX - startX}px`;
+      let x = e.pageX - startX;
+      let y = e.pageY - startY;
+
+      if (y < BOTTOM) {
+        y = 0;
+      }
+      if (x > LEFT) {
+        x = 0;
+      }
+      if (y > TOP) {
+        y = TOP;
+      }
+      if (x < RIGTH) {
+        x = RIGTH;
+      }
+      target.style.transform = `translate(${x}px, ${y}px)`;
     }
   };
 
@@ -92,7 +119,8 @@ export const Map = () => {
 
   return (
     <MapContext.Provider value={{markers, menuItem}}>
-      <div onMouseMove={(e) => mouseMoveHandler(e)}
+      <div
+        onMouseMove={(e) => mouseMoveHandler(e)}
         onMouseUp={mouseUpHandler}
         className={styles.map}
       >
