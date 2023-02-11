27b13072b3c5ba20238b2be8eb113d5e66affbf0
SergioAJS <SergFanMadrid@gmail.com>
2023-02-11 22:38:45 +0500

    feat: add post room method

--- gf-server-api/src/index.ts
+++ gf-server-api/src/index.ts
@@ -11,8 +11,10 @@ config();
 const app: Express = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/swagger.json", (_req, res) => res.json(apiSpec));
+app.post("/swagger.json", (_req, res) => res.json(apiSpec));
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(apiSpec));
 
 // App status check
