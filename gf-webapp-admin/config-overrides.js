033a1fa3269f06604d2bd46786b0cb0e64b1d7ef
SergioAJS <SergFanMadrid@gmail.com>
2023-02-18 10:16:37 +0500

    feat: add adaptive styles to header

--- gf-webapp-admin/config-overrides.js
+++ gf-webapp-admin/config-overrides.js
@@ -1,7 +1,8 @@
-import * as path from "path";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
 
-export default{
-  paths: function (paths) {
+module.exports = {
+  paths: function (paths, env) {
     paths.appSrc = path.resolve(__dirname);
     return paths;
   },
