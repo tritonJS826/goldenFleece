9d8ee6fb138f75cce0322d0ad1ea4fd137ade0aa
tritonJS826 <triton.68@mail.ru>
2023-03-09 09:54:19 +0300

    34 Autogenerating .env by env.example

---
+++ scripts/generateEnvAll.ts
@@ -0,0 +1,42 @@
+import path from "path";
+import fs from "fs";
+
+const GF_SERVER_API_PATH = path.resolve(__dirname, "../gf-server-api");
+const GF_SERVER_RESOURCES_PATH = path.resolve(__dirname, "../gf-server-resources");
+const GF_WEBAPP_PATH = path.resolve(__dirname, "../gf-webapp");
+const GF_WEBAPP_ADMIN_PATH = path.resolve(__dirname, "../gf-webapp-admin");
+
+const MODULES_PATHS = [
+  GF_SERVER_API_PATH,
+  GF_SERVER_RESOURCES_PATH,
+  GF_WEBAPP_PATH,
+  GF_WEBAPP_ADMIN_PATH,
+];
+
+const generateEnvIfNotExist = (modulePath: string) => {
+  const envPath = path.resolve(modulePath, ".env");
+  const envExamplePath = path.resolve(modulePath, "env.example");
+
+  const isEnvExist = fs.existsSync(envPath);
+  const isEnvExampleExist = fs.existsSync(envExamplePath);
+
+  if (!isEnvExampleExist) {
+    console.error(`env.example file by address ${envExamplePath} is not exist`);
+    return;
+  }
+
+  if (isEnvExist) {
+    console.log(`.env file by address ${envPath} already exist`);
+    return;
+  }
+
+  if (isEnvExampleExist && !isEnvExist) {
+    fs.copyFileSync(envExamplePath, envPath);
+    console.log(`.env file by address ${envPath} successfully created`);
+  }
+};
+
+
+console.log(`Generating .env files in modules: \n${MODULES_PATHS.join("\n")} \n`);
+
+MODULES_PATHS.forEach(generateEnvIfNotExist);
--- scripts/swaggerGenerateAll.ts
+++ scripts/swaggerGenerateAll.ts
@@ -1,11 +1,5 @@
-import {exec, ExecException} from "child_process";
-
-const errorHandler = (error: ExecException | null, stdout: string, stderr: string) => {
-  if (error || stderr) {
-    console.log(stdout);
-    console.error(error, stderr);
-  }
-};
+import {exec} from "child_process";
+import {errorHandler} from "./utils/errorHandler";
 
 exec(
   "pnpm run \
