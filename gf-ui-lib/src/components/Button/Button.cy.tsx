7703d6a040dbf0a2ec18ee7f7b283763192add67
tritonJS826 <56690686+tritonJS826@users.noreply.github.com>
2023-04-06 18:17:14 +0400

    Add basic tests (#96) (#113)
    * Fix swagger (#100)
    
    * add swagger comments
    
    * some changes
    
    * fix swagger autogenerated files
    
    * change scripts for generate swagger into src, fix swagger comments for generate models, fix paths for imports, fix models in services
    
    * add info to swagger, change models in admin and client
    
    * delete any and as unknown
    
    * fix problem with as
    
    * fix service layer in webapp-admin
    
    * fix eslint mistake
    
    * rename vars in RoomApi and formatted code
    
    * back changes in Adults.tsx
    
    * delete double ()
    
    * separate CI scripts
    
    * update CI scripts
    
    * update CI scripts
    
    * update CI scripts
    
    * update Ci scripts
    
    ---------
    
    Co-authored-by: Ekaterina <57002521+Ekaterina1994@users.noreply.github.com>

--- gf-ui-lib/src/components/Button/Button.cy.tsx
+++ gf-ui-lib/src/components/Button/Button.cy.tsx
@@ -1,18 +1,89 @@
 import {Button} from "./Button";
 import {mount} from "cypress/react";
+import styles from "./Button.module.scss";
 
-const BUTTON_VALUE = "TEST BUTTON";
-const STUB_FUNCTION = () => null;
+const STUB_BUTTON_VALUE = "TEST BUTTON";
 
-describe("<Button />", () => {
-  it("renders", () => {
-    // see: https://on.cypress.io/mounting-react
+describe("Button component", () => {
+  it("contains right value", () => {
+    const STUB_FUNCTION = cy.stub();
     mount(
       <Button
-        value={BUTTON_VALUE}
+        value={STUB_BUTTON_VALUE}
         onClick={STUB_FUNCTION}
       />,
     );
-    cy.get("button").should("contains.text", BUTTON_VALUE);
+    cy.get("button").should("contains.text", STUB_BUTTON_VALUE);
+  });
+
+  it("has a default type of \"button\" if type prop is not defined", () => {
+    const STUB_FUNCTION = cy.stub();
+    mount(
+      <Button
+        value={STUB_BUTTON_VALUE}
+        onClick={STUB_FUNCTION}
+      />,
+    );
+    cy.get("button").should("have.attr", "type", "button");
+  });
+
+  it("button type is set correctly if type prop is defined", () => {
+    const STUB_FUNCTION = cy.stub();
+    mount(
+      <Button
+        value={STUB_BUTTON_VALUE}
+        onClick={STUB_FUNCTION}
+        type="submit"
+      />,
+    );
+    cy.get("button[type=\"submit\"]").should("exist");
+  });
+
+  it("button size is set correctly if size prop is defined as 'innerContent'", () => {
+    const STUB_FUNCTION = cy.stub();
+    mount(
+      <Button
+        value={STUB_BUTTON_VALUE}
+        onClick={STUB_FUNCTION}
+        size="innerContent"
+      />,
+    );
+    cy.get("button").should("have.class", styles.innerContent);
+  });
+
+  it("button size is set correctly if size prop is not defined and it has 'parentWidth' style", () => {
+    const STUB_FUNCTION = cy.stub();
+    mount(
+      <Button
+        value={STUB_BUTTON_VALUE}
+        onClick={STUB_FUNCTION}
+      />,
+    );
+    cy.get("button").should("have.class", styles.parentWidth);
+  });
+
+  it("onClick callback is triggered when the button is clicked", () => {
+    const STUB_FUNCTION = cy.stub();
+    mount(
+      <Button
+        value={STUB_BUTTON_VALUE}
+        onClick={STUB_FUNCTION}
+      />,
+    );
+
+    cy.get("button").click().then(() => {
+      expect(STUB_FUNCTION).to.be.called;
+    });
+  });
+
+  it("button has the correct CSS classes applied", () => {
+    const STUB_FUNCTION = cy.stub();
+    mount(
+      <Button
+        value={STUB_BUTTON_VALUE}
+        onClick={STUB_FUNCTION}
+      />,
+    );
+    cy.get("button").should("have.class", styles.button);
   });
 });
--- gf-ui-lib/src/components/Loader/Loader.cy.tsx
+++ gf-ui-lib/src/components/Loader/Loader.cy.tsx
@@ -1,9 +1,36 @@
-import {mount} from "cypress/react";
+import {mount} from "cypress/react18";
 import {Loader} from "./Loader";
+import styles from "./Loader.module.scss";
 
-describe("<Loader />", () => {
+
+describe("Loader component", () => {
   it("renders", () => {
-    // see: https://on.cypress.io/mounting-react
     mount(<Loader />);
   });
+
+  it("shows loader when isLoading is true", () => {
+    mount(<Loader isLoading={true} />);
+    cy.get(`.${styles.loader}`).should("be.visible");
+  });
+
+  it("hides loader when isLoading is false", () => {
+    mount(<Loader isLoading={false} />);
+    cy.get(`.${styles.loader}`).should("not.exist");
+  });
+
+  it("returns null when isLoading is false", () => {
+    mount(<Loader isLoading={false} />);
+    cy.get(`.${styles.loader}`).should("not.exist");
+  });
+
+  it("sets default value of isLoading to true", () => {
+    mount(<Loader />);
+    cy.get(`.${styles.loader}`).should("be.visible");
+  });
+
+  it("displays correct title", () => {
+    mount(<Loader />);
+    cy.get(`.${styles.loader}`).contains("Golden Fleece");
+  });
+
 });
